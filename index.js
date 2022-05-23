92b3f23be61d16314781ae1b30d32af0c08c3c7c
Mohammad Jahid <mohammadjahid0007@gmail.com>
2022-05-24 02:14:46 +0600

    loaded all datas from database by get method

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 
@@ -18,18 +18,36 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 
-
-
-
 async function run() {
     try {
         await client.connect();
         const toolsCollection = client.db("ventrac").collection("tools");
+        const ordersCollection = client.db("ventrac").collection("orders");
 
+
+        //get all tools
         app.get('/tools', async (req, res) => {
             const tools = await toolsCollection.find({}).toArray();
             res.send(tools)
+        });
+
+        //get a single tool by id
+        app.get('/tools/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const tool = await toolsCollection.findOne(filter);
+            res.send(tool)
+        });
+
+        //post orders
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            console.log(order);
+            const result = await ordersCollection.insertOne(order);
+            res.send(result);
         })
+
+
 
     } finally {
 
