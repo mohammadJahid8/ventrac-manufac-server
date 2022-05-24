f5c7044ab0fdf0254a486fe1cd56f2c1f8f77d76
Mohammad Jahid <mohammadjahid0007@gmail.com>
2022-05-25 00:04:06 +0600

    made delete api

--- index.js
+++ index.js
@@ -89,6 +89,13 @@ async function run() {
       res.send(result);
     });
 
+    //Delete a order
+    app.delete("/order/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await ordersCollection.deleteOne(query);
+      res.send(result);
+    });
     //
   } finally {
   }
