cc8c1e78df38613c245b9cc0f93978301c3aa0c3
Mohammad Jahid <mohammadjahid0007@gmail.com>
2022-05-26 19:34:58 +0600

    api made for payment

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors());
 app.use(express.json());
@@ -39,6 +40,7 @@ async function run() {
     const ordersCollection = client.db("ventrac").collection("orders");
     const usersCollection = client.db("ventrac").collection("users");
     const reviewsCollection = client.db("ventrac").collection("reviews");
+    const paymentCollection = client.db("ventrac").collection("payments");
 
     //get all tools
     app.get("/tools", async (req, res) => {
@@ -61,11 +63,41 @@ async function run() {
       res.send(tool);
     });
 
+    //stripe
+    app.post("/create-payment-intent", verifyJWT, async (req, res) => {
+      const { price } = req.body;
+      const amount = price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+
     //post orders
     app.post("/orders", verifyJWT, async (req, res) => {
       const order = req.body;
       const result = await ordersCollection.insertOne(order);
       res.send(result);
+    });
+
+    //update orders
+    app.patch("/orders/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const payment = req.body;
+      const filter = { _id: ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          paid: true,
+          transactionId: payment.transactionId,
+        },
+      };
+      const result = await paymentCollection.insertOne(payment);
+      const updatedOrder = await ordersCollection.updateOne(filter, updateDoc);
+      res.send(updateDoc);
     });
 
     //post reviews
